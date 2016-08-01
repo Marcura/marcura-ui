7001b52da74fea551918fe81ba31782c85e63189
Evgenii Koriakin <eakoryakin@gmail.com>
2016-08-01 16:48:35 +0400

    Add a fallback for 'display: flex' for IE9

--- marcura-ui/marcura-ui.js
+++ marcura-ui/marcura-ui.js
@@ -1,3 +1,21 @@
 angular.module('marcuraUI.services', []);
 angular.module('marcuraUI.components', ['marcuraUI.services']);
 angular.module('marcuraUI', ['marcuraUI.components']);
+
+// Detect IE9
+angular.element(document).ready(function() {
+    var ie = (function() {
+        var version = 3,
+            div = document.createElement('div'),
+            all = div.getElementsByTagName('i');
+
+        while (div.innerHTML = '<!--[if gt IE ' + (++version) + ']><i></i><![endif]-->', all[0]);
+
+        return version > 4 ? version : null;
+    }());
+
+    if (ie) {
+        var body = angular.element(document.getElementsByTagName('body')[0]);
+        body.addClass('ma-ie' + ie);
+    }
+});
