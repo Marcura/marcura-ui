60410533efb046326210db47ad4f4ceda3bb3fe0
Evgenii Koriakin <eakoryakin@gmail.com>
2016-08-02 16:04:58 +0400

    Add Heroku config

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/app'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/dest', express.static(__dirname + '/dest'));
+
+// Set views directory.
+app.set('views', __dirname + '/app');
+
+// Set routes.
+app.get('/', function(request, response) {
+    response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
