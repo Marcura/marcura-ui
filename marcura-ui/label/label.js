d69e9f0a42ac87d7e40201f8649a71a18c57004c
Evgenii Koriakin <eakoriakin@yandex.ru>
2017-03-12 14:59:33 +0400

    Label: Improve styles

--- marcura-ui/label/label.js
+++ marcura-ui/label/label.js
@@ -9,7 +9,9 @@ angular.module('marcuraUI.components').directive('maLabel', [function () {
         replace: true,
         template: function () {
             var html = '\
-                <div class="ma-label">\
+                <div class="ma-label" ng-class="{\
+                    \'ma-label-is-required\': isRequired\
+                }">\
                     <label class="ma-label-text" for="{{for}}"><ng-transclude></ng-transclude></label><div class="ma-label-star" ng-if="isRequired">*</div>\
                 </div>';
 
