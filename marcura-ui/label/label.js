bf6b50d15abeb67b0a8f6e2f94748308eed62dcc
Evgenii Koriakin <eakoriakin@yandex.ru>
2018-03-12 12:12:23 +0400

    Label: Add hasHint parameter

--- marcura-ui/label/label.js
+++ marcura-ui/label/label.js
@@ -6,7 +6,8 @@ angular.module('marcuraUI.components').directive('maLabel', [function () {
             cutOverflow: '=',
             for: '@',
             isRequired: '=',
-            hasWarning: '='
+            hasWarning: '=',
+            hasHint: '='
         },
         replace: true,
         template: function () {
@@ -15,12 +16,13 @@ angular.module('marcuraUI.components').directive('maLabel', [function () {
                     \'ma-label-is-required\': isRequired,\
                     \'ma-label-has-content\': hasContent,\
                     \'ma-label-has-warning\': hasWarning,\
+                    \'ma-label-has-hint\': hasHint,\
                     \'ma-label-cut-overflow\': cutOverflow\
                 }">\
                     <label class="ma-label-text" for="{{for}}"><ng-transclude></ng-transclude></label><!--\
                     --><div class="ma-label-star" ng-if="isRequired">&nbsp;<i class="fa fa-star"></i></div><!--\
                     --><div class="ma-label-warning" ng-if="hasWarning">&nbsp;\
-                    <i class="fa fa-exclamation-triangle"></i></div>\
+                    <i class="fa fa-exclamation-triangle"></i></div><div class="ma-label-hint" ng-if="hasHint">&nbsp;<div class="ma-label-hint-inner"><i class="fa fa-question"></i><div></div>\
                 </div>';
 
             return html;
